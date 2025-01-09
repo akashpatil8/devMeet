c540259d74df73fb7b4297dbbeb64d6a53cd815b
Akash Patil <akashpatil4659@gmail.com>
2025-01-10 02:54:55 +0530

    create api for user signup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Swapnil",
+    lastName: "Patil",
+    email: "swapnil@gmail.com",
+    password: "swapnil123",
+    gender: "male",
+    age: 25,
+  });
+
+  try {
+    await user.save();
+    res.send("User added successfully!");
+  } catch (err) {
+    res.status(400).send("Error while saving the user:", err.message);
+  }
+});
 
 connectDB()
   .then(() => {
