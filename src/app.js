030a19b28496af28a84aa4b402e01a379ec8c5c6
Akash Patil <akashpatil4659@gmail.com>
2025-01-10 02:16:55 +0530

    connect to mongoDB

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Listening to port 3000");
-});
+connectDB()
+  .then(() => {
+    console.log("connection successfully established");
+    app.listen(3000, () => {
+      console.log("Listening to port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://akashpatil8:rrZUGPkQMuHewvu6@akashpatil.9cnc1.mongodb.net/devMeet"
+  );
+};
+
+module.exports = connectDB;
