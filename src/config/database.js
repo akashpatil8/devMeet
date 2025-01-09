70e7554916c170c418bd93023222cfd9399aae49
Akash Patil <akashpatil4659@gmail.com>
2025-01-10 02:32:22 +0530

    create user model

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://akashpatil8:rrZUGPkQMuHewvu6@akashpatil.9cnc1.mongodb.net/devMeet"
+    `mongodb+srv://akashpatil8:rrZUGPkQMuHewvu6@akashpatil.9cnc1.mongodb.net/devMeet`
   );
 };
 
