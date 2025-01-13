c4e4fa45ae774003490ed1ec5ec272e730521bbb
Akash Patil <akashpatil4659@gmail.com>
2025-01-13 14:06:36 +0530

    add data validation in user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,48 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  email: { type: String },
-  password: { type: String },
-  age: { type: Number },
-  gender: { type: String },
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+      trim: true,
+    },
+    lastName: { type: String, minLength: 3, maxLength: 50, trim: true },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: { type: String, required: true },
+    age: { type: Number, min: 18, trim: true },
+    gender: {
+      type: String,
+      trim: true,
+      validate(value) {
+        if (!["Male", "Female", "Other"].includes(value)) {
+          throw new Error("Gender not valid");
+        }
+      },
+    },
+    about: {
+      type: String,
+      minLength: 100,
+    },
+    imageUrl: {
+      type: String,
+      default:
+        "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/man-user-circle-icon.png",
+    },
+    skills: { type: [String] },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const user = mongoose.model("User", userSchema);
 
