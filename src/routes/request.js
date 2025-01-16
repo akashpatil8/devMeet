a66cccf401b5b83089bd44562e0db20706b5a952
Akash Patil <akashpatil4659@gmail.com>
2025-01-17 02:01:15 +0530

    add connectionRequest api for sending connection requests

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,15 +1,44 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestRouter.get("/request", userAuth, async (req, res) => {
-  const { user } = req.body;
+requestRouter.post(
+  "/request/send/:status/:userId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const { _id: fromUserId } = req.body.user;
+      const { status, userId: toUserId } = req.params;
 
-  try {
-    res.send(user);
-  } catch (error) {
-    res.status(400).send("Error: " + error.message);
+      const toUser = await User.findById({ _id: toUserId });
+      if (!toUser)
+        return res.status(404).send({ message: "User is not present." });
+
+      if (fromUserId.toString() === toUserId)
+        throw new Error("Cannot send connection request to yourself");
+
+      const existingConnectionRequest = await ConnectionRequest.find({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest.length)
+        throw new Error("Connection request already exists!");
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+      res.send({ data });
+    } catch (error) {
+      res.status(400).send({ message: "Error: " + error.message });
+    }
   }
-});
+);
 
 module.exports = requestRouter;
